dcb26da4753b8f8b85a6ba0c45ce73877838033b
winnubstj <winnubstj@janelia.hhmi.org>
2022-06-22 14:59:59 -0400

    Updating treadmill video with histogram and clearer interface

--- Matlab code/treadmillVideo.m
+++ Matlab code/treadmillVideo.m
@@ -10,12 +10,6 @@ function treadmillVideo(COMport,varargin)
 % approximately as fast as the chips will offload their image data.  This
 % is for experimental setup only.
 %
-% May 10, 2010
-% Gus K. Lott III, PhD (lottg@janelia.hhmi.org)
-% Developed at HHMI - Janelia Farm Research Campus
-%
-%
-%COMport = 'COM3';
 
 %Cleanup from previously opened instances
 delete(findobj('tag','gvid'))
@@ -25,14 +19,25 @@ tic
 %Create UI and image to dump values into
 gui.fig=figure('tag','gvid','numbertitle','off','menubar','none','name','Fly Treadmill Video Monitor - Gus K Lott III, PhD - HHMI JFRC 2008');
 centerfig(gui.fig)
-gui.im=image(zeros(30,61)); 
-colormap gray
-gui.ax=get(gui.im,'parent');
-axis equal
-axis fill
-set(gca,'position',[0 0 1 1])
-set(gui.ax,'visible','off')
-
+% create axis.
+gui.ax_im1 = subplot(2,2,1,'DataAspectRatio',[1,1,1]);
+gui.ax_im2 = subplot(2,2,2,'DataAspectRatio',[1,1,1]);
+gui.ax_hist1 = subplot(2,2,3);
+gui.ax_hist2 = subplot(2,2,4);
+% set blank images.
+gui.im1=imshow( zeros(30,30),'Parent',gui.ax_im1,'DisplayRange',[0,1]);
+gui.im2=imshow( zeros(30,30),'Parent',gui.ax_im2,'DisplayRange',[0,1]);
+% set blank histogram.
+gui.hist1 = histogram(gui.ax_hist1, zeros(60),20,'Normalization','probability');
+gui.hist2 = histogram(gui.ax_hist2, zeros(60),20,'Normalization','probability');
+% Set titles.
+gui.ax_im1.Title.String = 'Camera 1 Image';
+gui.ax_im2.Title.String = 'Camera 2 Image';
+gui.ax_hist1.Title.String = 'Camera 1 Histogram';
+gui.ax_hist1.XLabel.String = 'Pixel Value';
+gui.ax_hist1.YLabel.String = 'Probability';
+gui.ax_hist2.Title.String = 'Camera 2 Histogram';
+gui.ax_hist2.XLabel.String = 'Pixel Value';
 %Connect to serial object
 s=serial(COMport);
 s.baudrate=1250000;
@@ -40,7 +45,7 @@ s.inputbuffersize=50000;
 fopen(s);
 
 %Create control button
-gui.vidgo=uicontrol('style','toggle','backgroundcolor','r','value',0,'string','Start Video','units','normalized','position',[0.01 0.01 0.25 0.05],'callback',{@vidstartstop,gui.fig,s});
+gui.vidgo=uicontrol('style','toggle','backgroundcolor','r','value',0,'string','Start Video','units','normalized','position',[0.01 0.01 0.15 0.05],'callback',{@vidstartstop,gui.fig,s});
 set(gui.fig,'deletefcn',{@fdel,s})
 
 set(gui.fig,'userdata',gui)
@@ -62,15 +67,22 @@ switch get(obj,'value')
         s.bytesavailablefcncount=900*2; %one frame of pixels from each camera
         s.bytesavailablefcnmode='byte';
         fopen(s)
-        set(gui.im,'visible','on')
         fwrite(s,[251,0]);
 end
 
 function fdel(obj,event,s)
-fwrite(s,[250,0]);
-fclose(s);
-delete(s);
-delete(obj);
+    fwrite(s,[250,0]);
+    fclose(s);
+    delete(s);
+    delete(obj);
+
+function updatehist(xnew,h)
+    [~,BinEdges] = histcounts(xnew,h.NumBins);
+    BinLimits = [min(BinEdges),max(BinEdges)];
+    h.Data= xnew;
+    h.BinEdges= BinEdges;
+    h.BinLimits= BinLimits;
+
 
 %Fires every 1800 bytes (one frame from each camera).  MCU sends frames at 20Hz
 function baf(obj,event,fig)
@@ -90,12 +102,14 @@ raw1=raw(2:2:end);
 %remove bit 7 from all bytes.  Remove bit 6 from first pixel
 raw0=raw0-128; raw0(1)=raw0(1)-64;
 raw1=raw1-128; raw1(1)=raw1(1)-64;
-b0=reshape(raw0,[30,30])*2; %Map linear vector into 30 colums by 30 rows
-% disp(b0(10,10));
-b1=reshape(raw1,[30,30])*2; %Map linear vector into 30 colums by 30 rows
-set(gui.im,'cdata',[b0,ones(30,1)*64*.8,b1]) %Update graphics
-
-
+raw0=raw0*2;
+raw1=raw1*2;
+% plot image
+gui.im1.CData = reshape(raw0,[30,30]); %Map linear vector into 30 colums by 30 rows
+gui.im2.CData = reshape(raw1,[30,30]); %Map linear vector into 30 colums by 30 rows
+% plot histogram
+updatehist(raw0,gui.hist1)
+updatehist(raw1,gui.hist2)
 % toc
 % drawnow
 
